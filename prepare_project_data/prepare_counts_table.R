f12bf254576b40c4d4a85d6420e69817c0626915
Leon Hornich <leonhornich@NoLocalHotspot.local>
2024-12-12 16:23:55 +0100

    added thesis data to reposirtory

---
+++ prepare_project_data/prepare_counts_table.R
@@ -0,0 +1,28 @@
+# Load necessary libraries
+library(dplyr)
+
+# Load in the data from counts.csv
+counts_data <- read.table("counts.csv",
+                          header = T,
+                          sep = ",")
+
+# Remove columns for sum.pXPR_003, LN229.REGORAFENIB and potentially.spike.in.CP0041.plasmid.if.possible
+counts_data <- counts_data[, -c(3, 8, 9)]
+
+# Remove all rows that contain NA values
+counts_data_no_na <- na.omit(counts_data)
+
+# Remove rows where second column entry isn't a substring of first column entry
+
+
+# Export the table to csv
+write.table(counts_data_no_na,
+            file = "counts_cleaned.tsv",
+            sep = "\t",
+            row.names = F,
+            quote = F)
+  
+  
+  
+  
+  
---
+++ project_results/testing_files/run6.sh
@@ -0,0 +1,11 @@
+~/bin/nextflow run crisprseq/. \
+-profile docker \
+--analysis screening \
+--count_table /home-link/zxoij30/bsc/DATA/counts_cleaned.tsv \
+--mle \
+--drugz \
+--contrasts /home-link/zxoij30/bsc/DATA/contrast.csv \
+--gpt_interpretation drugz,mle \
+--outdir ./resultsRun6 \
+-c /home-link/zxoij30/bsc/run6.config \
+-resume
